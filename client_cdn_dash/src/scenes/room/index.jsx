92503c190cba29b116aa8b8e577209de4d42613c
MansiGit <mrborole@gmail.com>
2023-05-06 14:32:43 -0400

    corrected client

---
+++ client_cdn_dash/src/scenes/room/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SocketComponent from '../../components/SocketComponent';
+
+const Room = (props) => {
+  // const location = useLocation();
+  // console.log(location.state)
+  return (
+    <div>
+      <SocketComponent/>
+    </div>
+  )
+};
+export default Room;
---
+++ client_dash_new/src/scenes/room/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import SocketComponent from '../../components/SocketComponent';
+
+const Room = (props) => {
+  // const location = useLocation();
+  // console.log(location.state)
+  return (
+    <div>
+      <SocketComponent/>
+    </div>
+  )
+};
+export default Room;
