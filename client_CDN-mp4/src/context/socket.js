456cc8b6a19ef1a2a6261c7c294e0c16cf5c15fa
MansiGit <mrborole@gmail.com>
2023-05-06 14:34:29 -0400

    removed files

--- client_CDN-mp4/src/context/socket.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import socketio from 'socket.io-client';
-
-export const socket = socketio.connect('http://3.91.52.183:5000');
-export const SocketContext = React.createContext();
