b79b7ec1183db4e4be4233cb9b221207c89e579a
Mansi Borole <31558571+MansiGit@users.noreply.github.com>
2023-05-06 15:55:01 -0400

    Update index.jsx

--- client_mp4/src/scenes/room/index.jsx
+++ client_mp4/src/scenes/room/index.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-import VideoPlayer from '../../components/VideoPlayer';
 import SocketComponent from '../../components/SocketComponent';
 
 const Room = (props) => {
